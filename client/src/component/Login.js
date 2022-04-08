f54ab2db2a39bb06fcd14a38d44df00ca29280d1
Weiwei Liang <liang.wen.wei.903@gmail.com>
2022-04-07 23:09:34 -0700

    added register modal

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './../App.css'
+
+import { Register } from './Register';
 
 export const Login = () => {
 
+  const [register, setRegister] = useState(false);
+
+  const registerButtonCallback = () => {
+    setRegister(prev => !prev)
+  }
+
   return (
-    <div className='Login'>
-        <header className='Header'>Outer Cloud sign in</header>
-        <div className='Form'>
-            <div>Email: </div>
-            <input type="text"></input>
-            <div>Password: </div>
-            <input type="text"></input>
-        </div>
-        <div className="Buttons">
-            <button>Sign in</button>
-            <button className='Rbutton'>Register</button>
-        </div>
-    </div>
+    <>
+      <div className='Login'>
+          <header className='Login_Header'>Outer Cloud sign in</header>
+          <div className='Login_Form'>
+              <div>Email: </div>
+              <input type="text"></input>
+              <div>Password: </div>
+              <input type="text"></input>
+          </div>
+          <div className="Login_Buttons">
+              <button>Sign in</button>
+              <button className='Login_Rbutton' onClick={registerButtonCallback}>Register</button>
+          </div>
+      </div>
+      <Register show={register} setShow={registerButtonCallback}/>
+    </>
   )
 }
---
+++ client/src/component/Register.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const Register = ({show, setShow}) => {
+  return (
+    <>
+        {show ? (
+            <div  className='Register_Background'>
+                <div className='Register'>
+                    <header className='Register_Header'>Outer Cloud Register</header>
+                    <div className='Register_Form'>
+                        <div>Username: </div>
+                        <input type="text"></input>
+                        <div>Email: </div>
+                        <input type="text"></input>
+                        <div>Password: </div>
+                        <input type="text"></input>
+                    </div>
+                    <div className="Register_Buttons">
+                        <button onClick={setShow}>Cancel</button>
+                        <button className='Register_Rbutton'>Register</button>
+                    </div>
+                </div>
+            </div>
+        ) : null}
+    </>
+  )
+}
